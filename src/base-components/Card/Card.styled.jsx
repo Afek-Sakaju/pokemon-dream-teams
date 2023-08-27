a4e895b49e4fc7c269fbb8fb587db134a59ad494
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-08-27 19:32:49 +0300

    increased font size of the card content

--- src/base-components/Card/Card.styled.jsx
+++ src/base-components/Card/Card.styled.jsx
@@ -8,79 +8,79 @@ import MuiGrow from "@mui/material/Grow";
 import MuiBox from "@mui/material/Box";
 
 export const MuiCard = styled(Card)(({ theme }) => ({
-  display: "flex",
-  height: "310px",
-  width: "370px",
-  margin: "0 20px",
+    display: "flex",
+    height: "310px",
+    width: "370px",
+    margin: "0 20px",
 
-  [theme.breakpoints.down("lg")]: {
-    height: "260px",
-    width: "300px",
-    margin: "0 10px",
-  },
-  [theme.breakpoints.down("md")]: {
-    height: "230px",
-    width: "260px",
-    margin: "0 5px",
-  },
-  [theme.breakpoints.down("sm")]: {
-    height: "230px",
-    width: "240px",
-    margin: 0,
-  },
-  [theme.breakpoints.down("xs")]: {
-    height: "210px",
-    width: "230px",
-    margin: 0,
-  },
+    [theme.breakpoints.down("lg")]: {
+        height: "260px",
+        width: "300px",
+        margin: "0 10px",
+    },
+    [theme.breakpoints.down("md")]: {
+        height: "230px",
+        width: "260px",
+        margin: "0 5px",
+    },
+    [theme.breakpoints.down("sm")]: {
+        height: "230px",
+        width: "240px",
+        margin: 0,
+    },
+    [theme.breakpoints.down("xs")]: {
+        height: "210px",
+        width: "230px",
+        margin: 0,
+    },
 }));
 
 export const CardContent = styled(MuiCardContent)``;
 
 export const CardMedia = styled(MuiCardMedia)(({ theme }) => ({
-  height: "150px",
-  objectFit: "contain",
+    height: "150px",
+    objectFit: "contain",
 
-  [theme.breakpoints.down("lg")]: {
-    height: "120px",
-  },
-  [theme.breakpoints.down("md")]: {
-    height: "90px",
-  },
-  [theme.breakpoints.down("sm")]: {
-    height: "80px",
-  },
+    [theme.breakpoints.down("lg")]: {
+        height: "120px",
+    },
+    [theme.breakpoints.down("md")]: {
+        height: "90px",
+    },
+    [theme.breakpoints.down("sm")]: {
+        height: "80px",
+    },
 }));
 
 export const CardActionArea = styled(MuiCardActionArea)``;
 
 export const TitleTypography = styled(MuiTypography)(({ theme }) => ({
-  fontSize: "1.2em",
+    fontSize: "1.2em",
 
-  [theme.breakpoints.down("md")]: {
-    fontSize: "1.1em",
-  },
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "1em",
-  },
+    [theme.breakpoints.down("md")]: {
+        fontSize: "1.1em",
+    },
+    [theme.breakpoints.down("sm")]: {
+        fontSize: "1em",
+    },
 }));
 
 export const ContentTypography = styled(MuiTypography)(({ theme }) => ({
-  fontSize: "0.8em",
+    fontSize: "1em",
 
-  [theme.breakpoints.down("md")]: {
-    fontSize: "0.7em",
-  },
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "0.6em",
-  },
+    [theme.breakpoints.down("md")]: {
+        fontSize: "0.9em",
+    },
+    [theme.breakpoints.down("sm")]: {
+        fontSize: "0.8em",
+    },
 }));
 
 export const Grow = styled(MuiGrow)``;
 
 export const Box = styled(MuiBox)`
-  max-height: 63px;
-  height: 63px;
-  overflow-y: auto;
-  margin-top: 20px;
+    max-height: 63px;
+    height: 63px;
+    overflow-y: auto;
+    margin-top: 20px;
 `;
