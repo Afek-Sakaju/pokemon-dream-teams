da6e312859aa5458800932436a19b399224c9a07
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-11-08 18:07:10 +0200

    improved UI a bit

--- src/base-components/Title/Title.styled.jsx
+++ src/base-components/Title/Title.styled.jsx
@@ -2,30 +2,31 @@ import { styled } from "@mui/material/styles";
 import MuiBox from "@mui/material/Box";
 
 export const Box = styled(MuiBox)(({ theme }) => ({
-  maxWidth: "100%",
-  position: "absolute",
-  top: 0,
-  [theme.breakpoints.down("xs")]: {
-    maxWidth: "210px",
-  },
+    width: "100%",
+    position: "absolute",
+    top: 0,
+    [theme.breakpoints.down("xs")]: {},
 }));
 
 export const HeaderText = styled("h1")(({ theme }) => ({
-  textAlign: "center",
-  fontSize: "1.85em",
-  color: "white",
-  userSelect: "none",
+    textAlign: "center",
+    fontSize: "1.85em",
+    color: "white",
+    userSelect: "none",
+    width: "100%",
+    margin: 0,
+    marginTop: "10px",
 
-  [theme.breakpoints.down("lg")]: {
-    fontSize: "1.7em",
-  },
-  [theme.breakpoints.down("md")]: {
-    fontSize: "1.25em",
-  },
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "1.1em",
-  },
-  [theme.breakpoints.down("xs")]: {
-    fontSize: "0.9em",
-  },
+    [theme.breakpoints.down("lg")]: {
+        fontSize: "1.7em",
+    },
+    [theme.breakpoints.down("md")]: {
+        fontSize: "1.25em",
+    },
+    [theme.breakpoints.down("sm")]: {
+        fontSize: "1.1em",
+    },
+    [theme.breakpoints.down("xs")]: {
+        fontSize: "0.9em",
+    },
 }));
--- src/components/PokemonCardsBox/PokemonCardsBox.styled.jsx
+++ src/components/PokemonCardsBox/PokemonCardsBox.styled.jsx
@@ -5,17 +5,17 @@ import MuiBox from "@mui/material/Box";
 import MyPokemonCard from "../PokemonCard/PokemonCard";
 
 export const Box = styled(MuiBox)(({ theme }) => ({
-  display: "flex",
-  justifyContent: "space-evenly",
-  flexWrap: "wrap",
-  marginTop: "150px",
-  gap: "20px",
+    display: "flex",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap",
+    marginTop: "140px",
+    gap: "20px",
 
-  [theme.breakpoints.down("xs")]: {
-    position: "absolute",
-    top: "110px",
-    right: "-5px",
-  },
+    [theme.breakpoints.down("xs")]: {
+        position: "absolute",
+        top: "110px",
+        right: "-5px",
+    },
 }));
 
 export const PokemonCard = styled(MyPokemonCard)``;
